1dbf32a7b4daaa9f22b0b16e52c2c146acbf00da
yuezhuwanfeng <2098185366@qq.com>
2023-03-01 22:01:36 +0800

    简单搭建一个http服务

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "fmt""net/http"
+)
 
 func main() {
-    fmt.Println("Hello, 世界")
+    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "Welcome to my website!")
+        fmt.Fprintln(w, r.URL.Query().Get("name"))
+        fmt.Fprintln(w, r.FormValue("email"))})
+        fs := http.FileServer(http.Dir("static/"))
+        http.Handle("/static/", http.StripPrefix("/static/", fs))
+        http.ListenAndServe(":8080", nil)
 }
